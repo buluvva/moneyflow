c22a099d88bf5dd84ba1cfa162f10834c8cf8a66
maksimpiven <vietnam-11@mail.ru>
2022-11-28 18:53:02 +0400

    parsing logic changes

--- db_tests.py
+++
@@ -1,4 +0,0 @@
-cur.execute("DROP TABLE  test")
-cur.execute("CREATE TABLE IF NOT EXISTS test(id serial PRIMARY KEY, text VARCHAR, test_value real)")
-cur.execute("INSERT INTO test(text,test_value) VALUES ('тестовая строка',123458.9)")
-conn.commit()

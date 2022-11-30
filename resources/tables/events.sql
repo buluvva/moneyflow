0eeb45653728e22eed5ad9248b7c1b8a8cc83383
maksimpiven <vietnam-11@mail.ru>
2022-11-30 17:15:47 +0400

    changed name of column

--- resources/tables/events.sql
+++ resources/tables/events.sql
@@ -2,7 +2,9 @@ CREATE TABLE events
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INT REFERENCES users (id),
-    type VARCHAR(255),
+    event_type VARCHAR(255),
+    cash_value float4,
     category_id INT REFERENCES categories (id),
     timestamp VARCHAR(255)
 );
+
